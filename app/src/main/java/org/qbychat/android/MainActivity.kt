252f0c3b34c611f59accc49566b6b174c71a5ec3
cubewhy <cubewhy@163.com>
2024-07-04 21:42:48 +0800

    init

---
+++ app/src/main/java/org/qbychat/android/MainActivity.kt
@@ -0,0 +1,147 @@
+package org.qbychat.android
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import org.qbychat.android.ui.theme.QMessengerMobileTheme
+
+@SuppressLint(
+    "UnusedMaterial3ScaffoldPaddingParameter"
+)
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QMessengerMobileTheme {
+                Surface {
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    val scrollBehavior =
+                        TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
+                            .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        topBar = {
+                            TopAppBar(
+                                colors = TopAppBarDefaults.topAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                    titleContentColor = MaterialTheme.colorScheme.primary
+                                ),
+                                title = {
+                                    Text(
+                                        "QMessenger Beta"
+                                    )
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            drawerState.apply {
+                                                if (isClosed) open() else close()
+                                            }
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Filled.List,
+                                            contentDescription = "Menu"
+                                        )
+                                    }
+                                }
+                            )
+                        },
+                        floatingActionButton = {
+                            FloatingActionButton(
+                                onClick = { },
+                            ) {
+                                Icon(Icons.Filled.Add, "Floating action button.")
+                            }
+                        },
+                        content = {
+                            ModalNavigationDrawer(
+                                drawerState = drawerState,
+                                drawerContent = {
+                                    ModalDrawerSheet {
+                                        Column(modifier = Modifier.padding(16.dp)) {
+                                            Text(text = "qby")
+                                            Text(text = "rentler@lunarclient.top", color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f))
+                                        }
+                                        HorizontalDivider()
+                                        NavigationDrawerItem(
+                                            icon = { Icon(imageVector = Icons.Outlined.Home, contentDescription = "Homepage") },
+                                            label = { Text(text = "主页") },
+                                            selected = true,
+                                            onClick = {}
+                                        )
+                                        HorizontalDivider()
+                                        NavigationDrawerItem(
+                                            icon = { Icon(imageVector = Icons.Outlined.Info, contentDescription = "About") },
+                                            label = { Text(text = "关于") },
+                                            selected = false,
+                                            onClick = { /*TODO*/ }
+                                        )
+                                    }
+                                },
+                                gesturesEnabled = true
+                            ) {}
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QMessengerMobileTheme {
+        FloatingActionButton(
+            onClick = { },
+        ) {
+            Icon(Icons.Filled.Add, "Floating action button.")
+        }
+    }
+}
