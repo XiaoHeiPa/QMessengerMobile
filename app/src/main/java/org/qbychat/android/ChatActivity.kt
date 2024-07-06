7af2d105743ef24d6ccb1c60f8703a65c66c7028
cubewhy <cubewhy@163.com>
2024-07-06 14:12:53 +0800

    chatbox v1

--- app/src/main/java/org/qbychat/android/ChatActivity.kt
+++ app/src/main/java/org/qbychat/android/ChatActivity.kt
@@ -7,22 +7,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import org.qbychat.android.ui.theme.QMessengerMobileTheme
 
@@ -60,7 +65,7 @@ class ChatActivity : ComponentActivity() {
                         )
                     },
                     bottomBar = {
-
+                        ChatBox()
                     }
 
                 ) { innerPadding ->
@@ -72,17 +77,30 @@ class ChatActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ChatBox(modifier: Modifier = Modifier) {
+    var chatBoxValue by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    Row {
+        TextField(
+            value = chatBoxValue,
+            onValueChange = { newValue ->
+                chatBoxValue = newValue // update text
+            },
+            placeholder = {
+                Text(text = "QwQ")
+            }
+        )
+        IconButton(onClick = { /*TODO: SendMessage*/ }) {
+            Icon(imageVector = Icons.AutoMirrored.Filled.Send, contentDescription = "Send message")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     QMessengerMobileTheme {
-        Greeting("Android")
     }
 }
