3441b90701cc9e2ccdbefd0bf826e7bc45fe4e8e
cubewhy <cubewhy@163.com>
2024-07-07 11:19:59 +0800

    login api (full ver)

--- app/src/main/java/org/qbychat/android/Entities.kt
+++ app/src/main/java/org/qbychat/android/Entities.kt
@@ -1,11 +1,41 @@
 package org.qbychat.android
 
-import java.io.Serializable
+import kotlinx.serialization.Serializable
 
+
+@Serializable
 data class Channel(
     val id: Int,
     val shownName: String,
     val name: String,
     val directMessage: Boolean,
     val preview: String
-): Serializable
+) : java.io.Serializable
+
+@Serializable
+enum class Role {
+    USER,
+    ADMIN
+}
+
+@Serializable
+data class Account(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val role: Role,
+    val registerTime: String,
+    val nickname: String,
+)
+
+@Serializable
+data class Authorize(
+    var username: String,
+    var role: String,
+    var token: String,
+    var email: String,
+    var expire: String
+)
+
+@Serializable
+data class RestBean<T>(val code: Int, val data: T, val message: String?)
--- app/src/main/java/org/qbychat/android/utils/QMessengerAPI.kt
+++ app/src/main/java/org/qbychat/android/utils/QMessengerAPI.kt
@@ -1,9 +1,13 @@
 package org.qbychat.android.utils
 
+import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.qbychat.android.Account
+import org.qbychat.android.Authorize
+import org.qbychat.android.RestBean
 
 private val httpClient = OkHttpClient.Builder()
     .build()
@@ -11,7 +15,7 @@ const val HTTP_PROTOCOL = "https://"
 const val WS_PROTOCOL = "wss://"
 const val BACKEND = "backend.lunarclient.top"
 
-fun login(username: String, password: String): String? {
+fun login(username: String, password: String): Authorize? {
     val body = "username=$username&password=$password".toRequestBody("application/x-www-form-urlencoded".toMediaType())
     val request = Request.Builder()
         .url("$HTTP_PROTOCOL$BACKEND/user/login")
@@ -21,6 +25,8 @@ fun login(username: String, password: String): String? {
         if (this.body == null) {
             return null
         }
-        return this.body!!.string() // todo json
+        val response = Json.decodeFromString<RestBean<Authorize>>(this.body!!.string())
+        if (response.code != 200) return null
+        return response.data
     }
 }
