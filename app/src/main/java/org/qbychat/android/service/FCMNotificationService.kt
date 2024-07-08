c1c43dc9de8aa0a791d7c366f21fc8dc0856d92b
cubewhy <cubewhy@163.com>
2024-07-09 07:51:49 +0800

    check fcm token

--- app/src/main/java/org/qbychat/android/service/FCMNotificationService.kt
+++ app/src/main/java/org/qbychat/android/service/FCMNotificationService.kt
@@ -1,7 +1,14 @@
 package org.qbychat.android.service
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 
 class FCMNotificationService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
 
+        Log.v(TAG, token)
+//        sendRegistrationToServer(token)
+    }
 }
